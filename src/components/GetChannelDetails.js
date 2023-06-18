e496918f62565c5232b03a14fe44bdc04d30998f
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-06-18 14:30:06 -0400

    added: conditional statement allowing channel  data to render when the streamer is offline and livestreamer object does not exist

--- src/components/GetChannelDetails.js
+++ src/components/GetChannelDetails.js
@@ -8,7 +8,7 @@ const GetChannelDetails = () => {
   useEffect(() => {
     const fetchData = async () => {
         try {
-          const response = await axios.get('https://kick.com/api/v1/channels/hyubsama');
+          const response = await axios.get('https://kick.com/api/v1/channels/BruceDropemOff');
           setData(response.data);
         } catch (error) {
           console.error('Error fetching data:', error);
@@ -16,31 +16,39 @@ const GetChannelDetails = () => {
       };
       fetchData();
     }, []);
-        //Data array
-        console.log(data)
-        // channel
-        const channel = data.slug
-          console.log(channel)
+    
+          let channel;
+          let isOnline;
+          let activeOrBanned;
+          let liveViewerCount;
 
-        const isUserbanned = data.is_banned === true ? <p>Banned</p> : <p>Active</p>;
-          console.log(isUserbanned.props.children);
-
-        const isActive = data.livestream === null ? <p>This channel is offline</p> : <p>Online</p>;
-          console.log(isActive.props.children)
-
-        const livestreamViewers = data.livestream;
-          console.log(livestreamViewers)
-
-        const viewerCount = data.livestream.viewers;
-          console.log(viewerCount);
+          if(data){
+            console.log(data)
+              channel = data.slug
+                console.log(channel);
+    
+              isOnline = data.livestream === null ? <p>This channel is offline</p> : <p>Online</p>;
+                console.log(isOnline.props.children)
+              
+              activeOrBanned = data.is_banned === true ? <p>Banned</p> : <p>Active</p>;
+                console.log(activeOrBanned.props.children);
+            }
+            
+            if(data.livestream){
+              liveViewerCount = data.livestream.viewer_count;
+                console.log(liveViewerCount);
+            } else{
+              liveViewerCount = 0;
+            }
+            
 
         return (
-            <div className='live-stream-card'>
+          <div className='live-stream-card'>
               <ul>
-                <li>Channel: {data.slug}</li>
-                <li>{isUserbanned}</li>
-                <li>Status: {isActive}</li>
-                <li>Viewers: {viewerCount}</li>
+                <li>{channel}</li>
+                <li>{isOnline}</li>
+                <li>{activeOrBanned}</li>
+                <li>{liveViewerCount}</li>
               </ul>
             </div>
     )
