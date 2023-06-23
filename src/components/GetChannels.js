8d9c3e933387fe616dd097b94fe999b6f278c2f1
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-06-23 16:12:37 -0400

    added: Drdisrespect and Ludwig to streamers array & er ror handling for channels that have never gone live and dont have a previous/current title or profile pic uploaded

--- src/components/GetChannels.js
+++ src/components/GetChannels.js
@@ -12,7 +12,7 @@ let followers;
 let followerCount;
 let previousStreamTitle;
 
-let kickAvatar = 'https://dbxmjjzl5pc1g.cloudfront.net/3b83fba0-3fe7-4538-ae3f-3e95592706ec/images/user-profile-pic.png'
+let kickAvatar = 'https://dbxmjjzl5pc1g.cloudfront.net/3b83fba0-3fe7-4538-ae3f-3e95592706ec/images/user-profile-pic.png';
 
 const GetChannels = () => {
   const [data, setData] = useState([]);
