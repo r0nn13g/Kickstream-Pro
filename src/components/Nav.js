cbfae72114869234cf0039641a9e94dd9da00f8b
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-14 00:50:29 -0400

    added link to home on nav home logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return(
     <div className="nav">
+      <Link to='/dev'>
       <img id="nav-logo"src={'https://i.imgur.com/fExb69W.png'} alt="kickster"/>
+      </Link>
     </div>
   )
 }
