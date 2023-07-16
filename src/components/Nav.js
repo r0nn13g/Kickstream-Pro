626817c92130f9e82b5e37ef4c3c84ce2c7a0c6e
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-16 02:53:16 -0400

    styling improvements

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import {DensityMediumIcon} from '@mui/icons-material';
 
 const Nav = () => {
   return(
     <div  className="nav-bar">
-      <Link to='/dev'>
-      <img id="nav-logo"src={'https://i.imgur.com/fExb69W.png'} alt="kickster"/>
+      <Link id="nav-logo-link" to='/dev'>
+      <img id="nav-logo" src={'https://i.imgur.com/fExb69W.png'} alt="kickster"/>
+      {/* <img id="nav-logo-right" src={'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/120px-Hamburger_icon_white.svg.png?20190820131613'} alt="kickster"/> */}
       </Link>
+
     </div>
   )
 }
