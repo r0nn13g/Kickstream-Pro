0374fe188b9a30614912ac6fe007094baa54de93
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-17 15:03:26 -0400

    Added conditional rendering to display a rotating spinner animation when waiting for succesfull get requests

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { streamers } from "../data/streamers";
 import PulsatingDot from './PulsatingDot';
 import VideocamOffIcon from '@mui/icons-material/VideocamOffOutlined';
+import RotatingSpinner from "../components/RotatingSpinner";
 
 // Declare variables to store data
   let pfp;
@@ -30,6 +31,8 @@ import VideocamOffIcon from '@mui/icons-material/VideocamOffOutlined';
 const Channels = () => {
      // State to store the sorted data
      const [data, setData] = useState([]);
+     const [isLoading, setIsLoading] = useState(true);
+
      useEffect(() => {
        const fetchData = async () => {
          try {
@@ -41,11 +44,12 @@ const Channels = () => {
                return (b?.livestream?.viewer_count || 0) - (a?.livestream?.viewer_count || 0);
              });
               setData(sortedData);
+              setIsLoading(false); 
           } catch (error) {
             // Ignore 404 response and return null for this URL
             if (error.response && error.response.status === 404) {
               console.error("Banned channel in streamers Array", error)
-              return null;
+                 setIsLoading(true);
             } else {
               throw error; // Throw other errors
             }
@@ -57,29 +61,32 @@ const Channels = () => {
        }, []);
 
         return (
-          <div className="live-stream-card-container">
-          {/* Map over the data and render the live stream cards  */}
-         {data.map((item,index) => {
-           console.log(item)
-           //if verified object exists than a channel is verified and the verified variable is set to true
-            if(item.verified !== null){
-                verified = true;
-                console.log("verified user", verified);
-              } else {
-                verified = false;
-                console.log("verified user", verified);
-              }
-
-              //if item exists, set variables for channel name, followers, and previousStream titles
-              if(item && item.user && item.previous_livestreams[0]){
-                pfp = item.user.profile_pic;
-                channel = item.user.username;
-                followerCount = item.followersCount;
-                followers = followerCount.toLocaleString("en-US");
-                previousStreamTitle = item.previous_livestreams[0].session_title
-                slug = item.slug;
-              } else {
-                pfp = item.user.profile_pic;
+            <div className="live-stream-card-container">
+                    {isLoading ? (
+                      <RotatingSpinner />
+                      ) : (
+                        <>
+            {data.map((item,index) => {
+              console.log(item)
+                      //if verified object exists than a channel is verified and the verified variable is set to true
+                      if(item.verified !== null){
+                        verified = true;
+                        console.log("verified user", verified);
+                      } else {
+                        verified = false;
+                        console.log("verified user", verified);
+                      }
+                      
+                      //if item exists, set variables for channel name, followers, and previousStream titles
+                      if(item && item.user && item.previous_livestreams[0]){
+                        pfp = item.user.profile_pic;
+                        channel = item.user.username;
+                        followerCount = item.followersCount;
+                        followers = followerCount.toLocaleString("en-US");
+                        previousStreamTitle = item.previous_livestreams[0].session_title
+                        slug = item.slug;
+                      } else {
+                        pfp = item.user.profile_pic;
                 channel = item.user.username;
                 followerCount = item.followersCount;
                 followers = followerCount.toLocaleString("en-US");
@@ -100,14 +107,16 @@ const Channels = () => {
                 slug = item.slug;
               } else {
                 viewerCount = null;
-              streamTitle = `${previousStreamTitle}`;
-            };
-            //if a profile pic does not exist and channel has never gone live, set channel name, followers, previous stream title, and profile pic to default kick avatar.
-            if(!item.user.profile_pic && !item.livestream ){
-              pfp = kickAvatar;
-            }
-                //if channel is live, display Pulsating dot
-                isLive = item.livestream === null ? <div id='offline-live'><VideocamOffIcon/></div> : <div id='online-live'><PulsatingDot /></div>; 
+                streamTitle = `${previousStreamTitle}`;
+              };
+
+              //if a profile pic does not exist and channel has never gone live, set channel name, followers, previous stream title, and profile pic to default kick avatar.
+              if(!item.user.profile_pic && !item.livestream ){
+                pfp = kickAvatar;
+              };
+
+            //if channel is live, display Pulsating dot
+            isLive = item.livestream === null ? <div id='offline-live'><VideocamOffIcon/></div> : <div id='online-live'><PulsatingDot /></div>; 
                 //if channel is partnered with kick display verified badge next to name
                 isVerified = verified === true ? <img id='verified-badge-online' src={verifiedBadge} alt='verification-badge'/> : null ;
                 //if channel is partnered with kick and offline display verified badge with gray scale filter
@@ -117,39 +126,41 @@ const Channels = () => {
                 titleLive = !item.livestream ? <h6 id='title-offline'>{streamTitle}</h6> : <h6 id='title-online'>{streamTitle}</h6>;
                 pfpLive = !item.livestream ? <img id='offline-pfp' src={pfp} alt='channel_pfp'/> : <img id='online-pfp' src={pfp} alt='channel_pfp'/>;
                 followersLive = item.livestream === null ? <p id='followers-offline'>{followers}</p> : <p id='followers-online'>{followers}</p>;
-
-               //jsx returning live stream card
-               return(
-                    <div key={index} className='live-stream-card'>
-                          <Link className='channel-pfp-container' to={`https://www.kick.com/${slug}`} path='relative'>
-                            <div className="pfp">
-                             {pfpLive}
-                            </div>
-                          </Link>
-                          
-                          <div  className='live-stream-details-container'>
-                            <div className='channel-name-container'>
+                
+                //jsx returning live stream card
+                return(
+                  <div key={index} className='live-stream-card'>
+                      <Link className='channel-pfp-container' to={`https://www.kick.com/${slug}`} path='relative'>
+                        <div className="pfp">
+                          {pfpLive}
+                        </div>
+                      </Link>
+                        <div  className='live-stream-details-container'>
+                          <div className='channel-name-container'>
                             {channelLive}
-                            </div> 
-                            <div className='followed-by-container'>
-                              <div id='followers'>
+                          </div> 
+                          <div className='followed-by-container'>
+                            <div id='followers'>
                               {followersLive}
-                              </div>
-                            </div>
-                            <div className="stream-title-container">
-                              {titleLive} 
                             </div>
                           </div>
-
+                          <div className="stream-title-container">
+                            {titleLive} 
+                          </div>
+                          </div>
                           <div className="is-live">
                               {isLive}
-                          <div className='live-viewers-count-container'>
-                            {viewerCount}
+                            <div className='live-viewers-count-container'>
+                              {viewerCount}
+                            </div>
                           </div>
-                          </div>
-                        </div>
+                    </div>   
                        )
-                   })}
+                       
+                      })
+                    }
+                    </>
+                    )}
            </div>
         );
     };
---
+++ src/components/RotatingSpinner.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+import '../Styles/RotatingSpinner.css'
+
+
+const RotatingSpinner = () =>{
+  return(
+    <div className='rotating-spinner'>
+    <Stack id="spinner" sx={{ color: 'grey.500' }} spacing={2} direction="row">
+      <CircularProgress color="inherit" />
+    </Stack>
+    <b>Loading</b>
+    </div>
+  )
+};
+
+export default RotatingSpinner;
