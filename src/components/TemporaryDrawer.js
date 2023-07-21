b968024fafe6676c53f23b66964ad6ff98372fae
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-21 01:30:09 -0400

    temporary drawer bug fix

--- src/components/TemporaryDrawer.js
+++ src/components/TemporaryDrawer.js
@@ -78,6 +78,7 @@ export default function TemporaryDrawer() {
             PaperProps={{
             sx: {
               backgroundColor: "#1b1b1be4",
+              marginBottom: '25px'
             }
             }}
             anchor={anchor}
