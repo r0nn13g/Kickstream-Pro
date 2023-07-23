0d17d78e71af5ea0b78f7177161f43e273741b6a
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-23 19:43:38 -0400

    happy sunday, i added a ticker at the top of trending channels page

---
+++ src/components/AdTicker.js
@@ -0,0 +1,20 @@
+import Ticker, { NewsTicker } from 'nice-react-ticker';
+import '../styles/ticker-styles.css'
+// import icon from './news-icon.png'; // add images, please make sure they can be set as src
+
+
+const AdTicker = () => {
+  return(
+    <div className="ticker">
+          <Ticker isNewsTicker={true}>
+            <NewsTicker id="1"  title="KICKSTER - live streaming solutions for futuristic beings   |   Create your own personalized watch list at www.kicksta.netlify.app/create   |   BruceDropEmOff & xQc sign 2yr / $100M deal with Kick   |   Who is the next big signing?" url=" https://metro.co.uk/2020/02/22/blue-passports-issued-brits-first-time-decades-next-months-12281012/?ito=newsnow-feed" />
+            {/* <NewsTicker id="1"  title="Create your own personalized watch list at https://kicksta.netlify.app/create" url=" https://metro.co.uk/2020/02/22/blue-passports-issued-brits-first-time-decades-next-months-12281012/?ito=newsnow-feed" />
+            <NewsTicker id="2"  title="BruceDropEmOff & xQc signs $100M deal with Kick" url=" https://metro.co.uk/2020/02/22/blue-passports-issued-brits-first-time-decades-next-months-12281012/?ito=newsnow-feed" />
+            <NewsTicker id="1"  title="Whos the next big signing?" url=" https://metro.co.uk/2020/02/22/blue-passports-issued-brits-first-time-decades-next-months-12281012/?ito=newsnow-feed" />
+             */}
+            </Ticker>
+    </div>
+  )
+}
+
+export default AdTicker;
--- src/pages/showChannels.js
+++ src/pages/showChannels.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Channels from "../components/Channels.js";
 import Footer from '../components/Footer.js';
+import AdTicker from "../components/AdTicker.js";
 
 
 const ShowChannels = () => {
     return(
       <React.Fragment>
+        <AdTicker />
         <Channels />
         <Footer />
     </React.Fragment>
