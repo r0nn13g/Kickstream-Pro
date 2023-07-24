3a5be2c4eaee812a70e6d3dbb6356df778de169d
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-24 10:28:50 -0400

    removed dependencies causing error on netlify deploy

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+
 import '../styles/carousel-styles.css';
 
 const MyCarousel = () => (
     <div className='carousel-container'>
-      <Carousel plugins={['autoplay']}>
-          {/* <img src={'https://i.imgur.com/fExb69W.png'} /> */}
+      {/* <Carousel plugins={['autoplay']}>
           <img id='carousel-img' src={'https://i.imgur.com/B2Pd350.png'} alt='sony x3000' />
           <img id='carousel-img' src={'https://i.imgur.com/DWPZ6aL.png'} alt='jetson nano' />
           <img id='carousel-img' src={'https://i.imgur.com/9L3K8t4.png'} alt='shark geek battery' /> 
-          {/* <img id='carousel-img' src={''} alt='night hawk' />  */}
-      </Carousel>
+      </Carousel> */}
     </div>
 );
 
