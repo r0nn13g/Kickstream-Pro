119c0e224d0172747fe5519fafe5b2b2b596cddb
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-07-29 22:31:11 -0400

    ui improvements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/homepage-styles.css';
-import Footer from "../components/Footer";
 import HomeAnimation from '../assets/LSS.gif';
 import MaterialCards from "../components/MaterialCards";
-import Carousel from '../components/Carousel'
 
 const Home = () => {  
   return(
@@ -16,9 +14,7 @@ const Home = () => {
             <img id="home-enter-logo" src={'https://i.imgur.com/L59bKvb.png'} alt='enter-button'/>
           </Link>
         </div>
-        <MaterialCards/>
-        <Carousel/>
-        <Footer/>
+            <MaterialCards/>
     </div>
   )
 };
