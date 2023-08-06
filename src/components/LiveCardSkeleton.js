5699cbede6bd3885e69d2aa2170d0c31e50ea748
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-06 14:15:33 -0400

    refactored LiveCardSkeleton into less lines and better readability

--- src/components/LiveCardSkeleton.js
+++ src/components/LiveCardSkeleton.js
@@ -3,213 +3,35 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import '../styles/live-card-skeleton-styles.css';
 import 'react-loading-skeleton/dist/skeleton.css'
 
-const LiveCardSkeleton = ({props}) => {
-  return (
-    <div className="live-stream-card-skeleton-container">
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
-    <div className="live-stream-card">
-     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <div style={{ margin: "5px", marginLeft: "10px" }}>
-     <Skeleton
-        circle
-        height="45px"
-        width="45px"
-        containerClassName="avatar-skeleton"
-      />
-      </div>
-    <div style={{zIndex:"2", margin:"-6px", marginLeft: "12px"}}>
-      <p>
-        <Skeleton count={2} />
-      </p>
-    </div>
-    <div className="viewers-skeleton">
-    <Skeleton />
-    </div>
-  </SkeletonTheme>
-    </div>
+const LiveCardSkeleton = ({ props }) => {
+  const numCards = 10; // Change this number to the desired number of cards
 
-    
+  const skeletonCards = Array.from({ length: numCards }, (_, index) => (
+    <div key={index} className="live-stream-card">
+      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+        <div style={{ margin: "5px", marginLeft: "10px" }}>
+          <Skeleton
+            circle
+            height="45px"
+            width="45px"
+            containerClassName="avatar-skeleton"
+          />
+        </div>
+        <div style={{ zIndex: "2", margin: "-6px", marginLeft: "12px" }}>
+          <p>
+            <Skeleton count={2} />
+          </p>
+        </div>
+        <div className="viewers-skeleton">
+          <Skeleton />
+        </div>
+      </SkeletonTheme>
     </div>
+  ));
+
+  return (
+    <div className="live-stream-card-skeleton-container">{skeletonCards}</div>
   );
-}
+};
 
 export default LiveCardSkeleton;
