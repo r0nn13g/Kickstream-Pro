6676424c3d0d2dce23e557ad89aec3f660c14072
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-07 21:30:52 -0400

    added Live to temp drawer

--- src/components/TemporaryDrawer.js
+++ src/components/TemporaryDrawer.js
@@ -30,9 +30,14 @@ export default function TemporaryDrawer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-          <ListItem style={{ display: 'flex', justifyContent: 'flex-end', textDecoration: 'none' , color: 'var(--gray-elements)' }} >
+          <ListItem style={{ display: 'flex', justifyContent: 'flex-end', textDecoration: 'none' , color: 'var(--green-elements)' }} >
               <Link to="/trending" style={{ textDecoration: 'none' , color: 'var(--white-elements)' }}>
               <b>Trending +</b>
+              </Link>
+          </ListItem>
+          <ListItem style={{ display: 'flex', justifyContent: 'flex-end', textDecoration: 'none' , color: 'var(--white-elements)' }} >
+              <Link to="/live" style={{textDecoration: 'none' , color: 'var(--white-elements)' }}>
+              <b>Live +</b>
               </Link>
           </ListItem>
           <ListItem style={{ display: 'flex', justifyContent: 'flex-end', textDecoration: 'none' , color: 'var(--green-elements)' }} >
