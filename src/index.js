8d0c02db0940effa35e89ee2d16305e2ca1bb2fc
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-17 16:59:11 -0400

    removed background color on ad ticker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home.js'
 import Navbar from './components/Navbar.js';
 import FourOhFour from './pages/FourOhFour.js';
-import ShowTrending from './pages/ShowTrending.js';
+// import ShowTrending from './pages/ShowTrending.js';
 import CreateChannels from './pages/CreateChannels.js';
 import Support from './pages/Support.js';
 import Live from './pages/ShowLive.js';
@@ -16,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path='*' element={<FourOhFour/>}></Route>
         <Route path='/' element={<Home />}></Route>
-        <Route path="/trending" element={<ShowTrending />}></Route>
+        {/* <Route path="/trending" element={<ShowTrending />}></Route> */}
         <Route path="/create" element={<CreateChannels />}></Route>
         <Route path="/support" element={<Support/>}></Route>
         <Route path="/Live" element={<Live />}></Route>
