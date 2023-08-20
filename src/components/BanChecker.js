55314cac84591111eabd8e1c1b080abb62292820
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-20 15:38:27 -0400

    created a basic structure for fetching data from api when a user inputs a streamers name

--- src/components/BanChecker.js
+++ src/components/BanChecker.js
@@ -3,8 +3,30 @@ import { Link } from "react-router-dom";
 import "../styles/ban-checker-styles.css"
 import axios from "axios";
 
-
 const BanChecker = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [streamerName, setStreamerName] = useState("");
+  
+  const fetchData = async (streamerName) => {
+      try {
+        const slug = streamerName.toLowerCase().replace(/\s/g, "");
+        const url = `https://kick.com/emotes/${slug}`;
+        const response = await axios.get(url);
+        const responseData = response.data;
+       
+        console.log(responseData);
+        setData(responseData)
+      } catch (error) {
+      console.log("Softbanned by kick servers  & cloud flare")
+      }
+  }
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      fetchData(streamerName);
+      setStreamerName("");
+    }
+
   
   return(
     <div className="ban-checker-container">
