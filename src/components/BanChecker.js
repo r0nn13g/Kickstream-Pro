53fcd9b418473f9e1dc5d9033acb6c405b85b3de
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-20 17:22:19 -0400

    added profile pic, username and ban status on ban checker component

--- src/components/BanChecker.js
+++ src/components/BanChecker.js
@@ -1,46 +1,69 @@
-import {React, useState, useEffect} from "react";
-import { Link } from "react-router-dom";
-import "../styles/ban-checker-styles.css"
 import axios from "axios";
+import React, { useState } from "react";
+import "../styles/ban-checker-styles.css";
 
 const BanChecker = () => {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [streamerName, setStreamerName] = useState("");
-  
-  const fetchData = async (streamerName) => {
-      try {
-        const slug = streamerName.toLowerCase().replace(/\s/g, "");
-        const url = `https://kick.com/emotes/${slug}`;
-        const response = await axios.get(url);
-        const responseData = response.data;
-       
-        console.log(responseData);
-        setData(responseData)
-      } catch (error) {
-      console.log("Softbanned by kick servers  & cloud flare")
-      }
-  }
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      fetchData(streamerName);
-      setStreamerName("");
-    }
 
-  
-  return(
-    <div className="ban-checker-container">
+  const fetchData = async (streamerName) => {
+    try {
+      const slug = streamerName.toLowerCase().replace(/\s/g, "");
+      const url = `https://kick.com/emotes/${slug}`;
+      const response = await axios.get(url);
+      const responseData = response.data;
+      setData([responseData]); // Wrap responseData in an array to correctly handle it
+    } catch (error) {
+      console.log("Softbanned by kick servers & cloud flare");
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData(streamerName);
+    setStreamerName("");
+  };
+
+  return (
+    <div className="ban-checker">
+      <div className="ban-checker-input-wrapper">
         <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={streamerName}
-                    onChange={(event) => setStreamerName(event.target.value)}
-                    placeholder="Enter streamer's name"
-                />
-                <button type="submit">Search</button>
-            </form>
+          <input
+            type="text"
+            value={streamerName}
+            onChange={(event) => setStreamerName(event.target.value)}
+            placeholder="Enter streamer's name"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      {data.map((item, index) => {
+        let pfp = ""; // Initialize with an empty string
+        let streamer = ""; // Initialize with an empty string
+        let isBanned = false; // Initialize as false
+
+        console.log(item[0]);
+        if (!item[0]) {
+          console.log("This streamer does not exist in our system yet");
+          return null; // Return null if the streamer doesn't exist
+        } else {
+          isBanned = item[0].is_banned;
+        }
+        
+        if (item[0].user) {
+          pfp = item[0].user.profile_pic; // Use item.user.profile_pic
+          streamer = item[0].user.username; // Use item.user.username
+        }
+
+        return (
+          <div key={index}>
+            <img src={pfp} alt="Profile" />
+            <p>{`${streamer}, has been banned: ${isBanned}`}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 };
 
 export default BanChecker;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar.js';
 import FourOhFour from './pages/FourOhFour.js';
 // import ShowTrending from './pages/ShowTrending.js';
 import CreateChannels from './pages/CreateChannels.js';
+import ShowChecker from './pages/ShowChecker.js';
 import Support from './pages/Support.js';
 import Live from './pages/ShowLive.js';
 
@@ -14,12 +15,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<FourOhFour/>}></Route>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<FourOhFour/>}/>
+        <Route path='/' element={<Home />}/>
         {/* <Route path="/trending" element={<ShowTrending />}></Route> */}
-        <Route path="/create" element={<CreateChannels />}></Route>
-        <Route path="/support" element={<Support/>}></Route>
-        <Route path="/Live" element={<Live />}></Route>
+        <Route path="/create" element={<CreateChannels />}/>
+        <Route path='/checker' element={<ShowChecker />}/>
+        <Route path="/support" element={<Support/>}/>
+        <Route path="/Live" element={<Live />}/>
       </Routes>
       <Navbar/>
     </BrowserRouter>
