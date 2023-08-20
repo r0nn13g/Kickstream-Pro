d8d9becebb99922037e70673a46bfe561ab330c2
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-19 23:01:50 -0400

    updated AdTicker, bug fixes and ui improvements

--- src/components/AdTicker.js
+++ src/components/AdTicker.js
@@ -4,8 +4,8 @@ import '../styles/ticker-styles.css';
 const AdTicker = () => {
   return(
     <div className="ticker">
-          <Ticker isNewsTicker={true}>
-            <NewsTicker id="2"  title="   r0nn13g       | Kickster® - live streaming solutions for futuristic beings   |   create your own personalized watch list   |   Keep track of trending channels without leaving the stream!   |   BruceDropEmOff & xQc sign 2yr / $100M deal with Kick   |   who is the next big signing?    |    r0nn13g" url="https://www.kick.com" />
+          <Ticker isNewsTicker={false}>
+            <NewsTicker id="2"  title="Kickster® - live streaming solutions for futuristic beings   |   create your own personalized watch list   |   Keep track of trending channels without leaving the stream!" url="https://www.kick.com" />
           </Ticker>
     </div>
   )
