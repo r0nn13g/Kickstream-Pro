a7359403642e301c702ad9be52bc50b9df3ea303
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-20 15:11:08 -0400

    initialized ban checker components, pages and style sheet

---
+++ src/components/BanChecker.js
@@ -0,0 +1,16 @@
+import {React, useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import "../styles/ban-checker-styles.css"
+import axios from "axios";
+
+
+const BanChecker = () => {
+  
+  return(
+    <div className="ban-checker-container">
+
+    </div>
+  )
+};
+
+export default BanChecker;
---
+++ src/pages/ShowChecker.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BanChecker from "../components/BanChecker";
+
+const ShowChecker = () => {
+  return (
+    <div className="ban-checker-wrapper">
+      <BanChecker />
+    </div>
+  )
+};
+
+export default ShowChecker;
