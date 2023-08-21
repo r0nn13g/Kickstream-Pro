a9f7b3a8bd46f042f4d6df64bc4f941852f25cb8
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-20 20:03:42 -0400

    edited trending style sheet name to live-styles

--- src/components/Live.js
+++ src/components/Live.js
@@ -1,6 +1,6 @@
 import {React, useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import '../styles/trending-styles.css';
+import '../styles/live-styles.css';
 import axios from "axios";
 import { streamers } from "../data/Streamers";
 import PulsatingDot from './PulsatingDot';
