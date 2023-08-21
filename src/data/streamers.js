c26b0591adcbf237f20e9d8e4226c18305ad183c
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-20 21:47:05 -0400

    added streamer to array

--- src/data/streamers.js
+++ src/data/streamers.js
@@ -6,6 +6,7 @@ export let streamers = [
     'https://kick.com/api/v1/channels/bongbong_irl',
     'https://kick.com/api/v1/channels/burgerplanet',
     'https://kick.com/api/v1/channels/brucedropemoff',
+    'https://kick.com/api/v1/channels/crazytawn',
     'https://kick.com/api/v1/channels/cristravels',
     'https://kick.com/api/v1/channels/deepak',
     'https://kick.com/api/v1/channels/eddie',
