195dfffa4c03c1be3e2f991c7689469fc28917bb
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-20 23:31:27 -0400

    enabled Trending page route

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home.js'
 import Navbar from './components/Navbar.js';
 import FourOhFour from './pages/FourOhFour.js';
-// import ShowTrending from './pages/ShowTrending.js';
+import ShowTrending from './pages/ShowTrending.js';
 import CreateChannels from './pages/CreateChannels.js';
 import ShowChecker from './pages/ShowChecker.js';
 import Support from './pages/Support.js';
@@ -17,7 +17,7 @@ export default function App() {
       <Routes>
         <Route path='*' element={<FourOhFour/>}/>
         <Route path='/' element={<Home />}/>
-        {/* <Route path="/trending" element={<ShowTrending />}></Route> */}
+        <Route path="/trending" element={<ShowTrending />}></Route>
         <Route path="/create" element={<CreateChannels />}/>
         <Route path='/checker' element={<ShowChecker />}/>
         <Route path="/support" element={<Support/>}/>
