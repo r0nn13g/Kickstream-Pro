3e6f74568f089e8d4e4f31b6907c2aef9114759b
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-26 19:22:43 -0400

    fixed yt clip ui on home page

--- src/components/DisplayVideo.js
+++ src/components/DisplayVideo.js
@@ -4,9 +4,9 @@ import YouTube from 'react-youtube';
 
 const DisplayVideo = () => {
     const { id } = useParams();
-
     return (
         <div className="videos">
+            <h3 style={{color: "red", textAlign: "center"}}>Youtube</h3>
             <YouTube videoId={id} opts={{ height: '200', width: '350' }} />
         </div>
     );
--- src/components/YTLoader.js
+++ src/components/YTLoader.js
@@ -7,7 +7,7 @@ const YTLoader = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=kick_clipz&type=video&key=${process.env.REACT_APP_API_KEY}`)
+        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=kick_clipz&type=video&key=${process.env.REACT_APP_API_KEY}`)
             .then((response) => {
                 setData(response.data.items);
             })
@@ -20,16 +20,16 @@ const YTLoader = () => {
         <div key={video.id.videoId} className="video-thumbnail-wrapper">
             <Link to={`/videos/${video.id.videoId}`}>
                 <img id="video-thumbnails" src={video.snippet.thumbnails.high.url} alt="youtube thumbnail" />
-                <br />
-                <h4 style={{ color: "var(--green-elements" }} id="video-title">{video.snippet.channelTitle}</h4>
                 <h4 id="video-title">{video.snippet.title}</h4>
+                <h4 style={{ color: "var(--green-elements", marginTop:"0px"}} id="video-title">{video.snippet.channelTitle}</h4>
             </Link>
         </div>
     ));
-
+    
     return (
         <div>
             <div className="videos">
+            <h6 style={{color: "var(--green-elements)", textAlign: "center"}}>Degen news provided by @Kick_Clipz</h6>
                 {youtubeDataArr}
             </div>
         </div>
