4d8811a681f0f5aead878f48e6e5a86452b4ae49
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-08-26 21:18:33 -0400

    ui improvements on yt loader

--- src/components/DisplayVideo.js
+++ src/components/DisplayVideo.js
@@ -7,7 +7,9 @@ const DisplayVideo = () => {
     return (
         <div className="videos">
             <h3 style={{color: "red", textAlign: "center"}}>Youtube</h3>
-            <YouTube videoId={id} opts={{ height: '200', width: '350' }} />
+            <div className="video-container">
+                <YouTube videoId={id} opts={{ height: '200', width: '350'}} />
+            </div>
         </div>
     );
 }
--- src/components/YTLoader.js
+++ src/components/YTLoader.js
@@ -21,7 +21,7 @@ const YTLoader = () => {
             <Link to={`/videos/${video.id.videoId}`}>
                 <img id="video-thumbnails" src={video.snippet.thumbnails.high.url} alt="youtube thumbnail" />
                 <h4 id="video-title">{video.snippet.title}</h4>
-                <h4 style={{ color: "var(--green-elements", marginTop:"0px"}} id="video-title">{video.snippet.channelTitle}</h4>
+                {/* <h4 style={{ color: "var(--green-elements", marginTop:"0px"}} id="video-title">{video.snippet.channelTitle}</h4> */}
             </Link>
         </div>
     ));
