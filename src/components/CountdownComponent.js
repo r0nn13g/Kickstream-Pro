efae668247eab8a399da00c3047680b5cc0f1ba8
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-14 10:48:44 -0400

    refactored countdown component for local tz

--- src/components/CountdownComponent.js
+++ src/components/CountdownComponent.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
+function getTimeZoneOffset() {
+  const now = new Date();
+  return now.getTimezoneOffset();
+}
+
 const CountdownComponent = () => {
   const [countdown, setCountdown] = useState({
     days: 0,
@@ -10,8 +15,15 @@ const CountdownComponent = () => {
   });
 
   useEffect(() => {
-    // Set the date we're counting down to
-    const countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+    function calculateCountDownDate() {
+      const timeZoneOffset = getTimeZoneOffset();
+      const targetDate = new Date("Sep 14, 2023 15:00:00");
+      const adjustedDate = new Date(targetDate.getTime() + timeZoneOffset * 60000);
+      return adjustedDate.getTime();
+    }
+
+    // Initial calculation of countdown date
+    const countDownDate = calculateCountDownDate();
 
     // Update the count down every 1 second
     const interval = setInterval(function () {
@@ -22,7 +34,7 @@ const CountdownComponent = () => {
       const distance = countDownDate - now;
 
       // Time calculations for days, hours, minutes, and seconds
-      const days = Math.floor(distance / (1000 * 60 * 60 * 12000));
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -35,7 +47,6 @@ const CountdownComponent = () => {
         setCountdown({ days, hours, minutes, seconds, expired: false });
       }
     }, 1000);
-
     return () => {
       // Cleanup on unmount
       clearInterval(interval); 
@@ -50,7 +61,7 @@ const CountdownComponent = () => {
         <div  style={{textAlign:"center", marginTop: '100px'}}>
           <img style={{height: "200px"}} src='https://media.discordapp.net/attachments/1124460443054919691/1146766843470688366/fruitenjoyer.gif?width=480&height=270' alt='fasdf'/>
         <span>
-          <div style={{textAlign:"center"}}><h3>Johnny Somali unbanned</h3></div>
+          <div style={{textAlign:"center"}}><h3>Johnny Somali Free in</h3></div>
         <div style={{textAlign:"center", color: countdown.expired ? "green" : "red" }}><h3>{countdown.days}d {countdown.hours}h {countdown.minutes}m {countdown.seconds}s</h3></div>
         </span>
       </div>
