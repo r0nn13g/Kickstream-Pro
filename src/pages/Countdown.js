68bc0d5d6364b4948046d453d80f5c8c609c5cbd
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-13 21:14:48 -0400

    added countdown page for countdown clock component

---
+++ src/pages/Countdown.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CountdownComponent from "../components/countdownComponent";
+
+const Countdown = () => {
+  return(
+    <div>
+      <CountdownComponent />
+    </div>
+  )
+}
+
+export default Countdown;
