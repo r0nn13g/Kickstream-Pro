96a20cd81a7f4889445f70efea2052ae8490f838
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-14 11:21:56 -0400

    removed ticker from live page

--- src/pages/ShowLive.js
+++ src/pages/ShowLive.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Live from "../components/Live.js";
 import Footer from '../components/Footer.js';
-import AdTicker from "../components/AdTicker.js";
 
 const ShowLive = () => {
   return (
     <div>
-      <AdTicker />
       <Live />
       <Footer />
     </div>
