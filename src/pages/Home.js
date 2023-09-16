7f77428a8c876c602d0d42d41f96f1db59102a7b
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-15 23:04:01 -0400

    bug fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/homepage-styles.css';
-import YTLoader from '../components/YTLoader.js';
 import Footer from "../components/Footer";
 import kicksterLogo from "../assets/LSS.gif";
-import AdTicker from '../components/AdTicker.js';
-import ClockWidget from "../components/ClockWidget";
 
 const Home = () => {  
   return(
     <div className="'homepage-container'">
-      <AdTicker />
-      <div className="home-text">
+      <div className="home-text" style={{marginTop: "180px"}}>
           <Link to={'/live'}>
             <img id="home-animation" src={kicksterLogo} alt="Live-streaming-solutions"/>
-            <div></div>
-            {/* <img id="home-enter-logo" src={'https://i.imgur.com/L59bKvb.png'} alt='enter-button'/> */}
           </Link>
-          <ClockWidget />
           <div></div>
         </div>
-          <YTLoader />
           <Footer/>
     </div>
   )
