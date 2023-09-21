92da7ac0e354ef2b20155dc2c944ca4cc316389f
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-20 21:47:41 -0400

    added error handling that solves an issue when the application is continously fetching while the current user ip is softbanned by cloudflare

--- src/components/Live.js
+++ src/components/Live.js
@@ -78,7 +78,7 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
           if (!loadingTimeout) {
             setLoadingTimeout(setTimeout(() => {
               setLoading(false);
-              console.log("Softbanned by Cloudflare & kick servers. please retry later.");
+              console.log("Softbanned by Cloudflare & kick servers. Retrying in a few moments.");
             }, 15000)); 
           }
         }
@@ -154,21 +154,22 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
             <h3 id="live-header-container">
               {showLive ? "LIVE" : "OFFLINE" }
             </h3>
-            {showLive ? <VideoCameraFrontIcon style={{ fill: showLive ? 'red' : 'gray' }}/> :  <VideoCamOffIcon style={{fill: showLive ? 'red' : 'gray'}}/> }            
+            {showLive ? <VideoCameraFrontIcon style={{ fill: showLive ? 'red' : 'gray' }}/> :  <VideoCamOffIcon style={{fill: showLive ? 'red' : 'gray'}}/> }
           </div>
         </div>
-        {isLoading ? (
+        { loadingTimeout ? (
+          <div className="live-stream-card-error" style={{textAlign: "center", margin:"200px 20px 0px 20px"}}>
+          <b style={{color: "var(--gray-elements)"}}>
+            oops.. there seems to be a problem. please retry later.
+          </b>
+        </div>
+        ) : isLoading ? (
           <LiveCardSkeleton />
           ) : (
             <>
             {showLive ? (
               // Display online streamers
               <>
-              {/* <div style={{textAlign: "center", margin:"200px 20px 0px 20px"}}>
-                <b style={{color: "var(--gray-elements)"}}>
-                  oops.. there seems to be a problem. please retry later.
-                </b>
-              </div> */}
                 {displayStreamers.map((item, index) => {
                   // if verified object exists than a channel is verified and the verified variable is set to true
                   verified = item.verified !== null;
@@ -179,14 +180,14 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
                   followerCount = item.followersCount;
                   followers = followerCount.toLocaleString("en-US");
                   previousStreamTitle = item.previous_livestreams[0]
-                    ? item.previous_livestreams[0].session_title
+                  ? item.previous_livestreams[0].session_title
                     : "No titles yet.";
                   slug = item.slug;
 
                   // if channel is live, populate raw viewers, followers, slug and stream title, else if channel is offline set stream title to the last stream title used by channel
                   rawViewers = item.livestream
-                    ? item.livestream.viewer_count
-                    : null;
+                  ? item.livestream.viewer_count
+                  : null;
                   viewerCount = rawViewers
                     ? rawViewers.toLocaleString("en-US")
                     : "Live";
@@ -197,8 +198,8 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
                   // if a profile pic does not exist and channel has never gone live, set channel name, followers, previous stream title, and profile pic to default kick avatar.
                   pfpLive = item.livestream ? (
                     <img id="online-pfp" src={pfp} alt="channel_pfp" />
-                  ) : (
-                    <img id="offline-pfp" src={pfp} alt="channel_pfp" />
+                    ) : (
+                      <img id="offline-pfp" src={pfp} alt="channel_pfp" />
                   );
 
                   // if channel is live, display Pulsating dot
@@ -218,17 +219,17 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
                       id="verified-badge-online"
                       src={verifiedBadge}
                       alt="verification-badge"
-                    />
-                  ) : null;
-                  
-                  // if channel is partnered with kick and offline display verified badge with gray scale filter
-                  verifiedLive =
-                    !item.livestream && isVerified ? (
-                      <img
+                      />
+                      ) : null;
+                      
+                      // if channel is partnered with kick and offline display verified badge with gray scale filter
+                      verifiedLive =
+                      !item.livestream && isVerified ? (
+                        <img
                         id="verified-badge-offline"
                         src={verifiedBadge}
                         alt="verification-badge"
-                      />
+                        />
                     ) : (
                       isVerified
                     );
@@ -256,12 +257,12 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
                   // Followers
                   followersLive = item.livestream ? (
                     <p id="followers-online">{followers} followers</p>
-                  ) : (
-                    <p id="followers-offline">{followers} followers</p>
-                  );
-
-                  // JSX returning live stream card
-                  return (
+                    ) : (
+                      <p id="followers-offline">{followers} followers</p>
+                      );
+                      
+                      // JSX returning live stream card
+                      return (
                     <div key={index} className="live-stream-card">
                       <Link
                         className="channel-pfp-container"
@@ -449,7 +450,7 @@ import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
               </>
             )}
           </>
-        )}
+          )}
       </div>
     );
   };
