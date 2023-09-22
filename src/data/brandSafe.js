e3cb59c4cbf866a8ce451f23220ad64cf301bbda
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-22 01:36:02 -0400

    added alternative brand safe streamers list

---
+++ src/data/brandSafe.js
@@ -0,0 +1,16 @@
+export let brandSafe = [
+  'https://kick.com/api/v1/channels/xqc',
+  'https://kick.com/api/v1/channels/adinross',
+  'https://kick.com/api/v1/channels/lospollostv',
+  'https://kick.com/api/v1/channels/yourrage',
+  'https://kick.com/api/v1/channels/brucedropemoff',
+  'https://kick.com/api/v1/channels/eddie',
+  'https://kick.com/api/v1/channels/trainwreckstv',
+  'https://kick.com/api/v1/channels/roshtein',
+  'https://kick.com/api/v1/channels/xposed',
+  'https://kick.com/api/v1/channels/mellstroy',
+  'https://kick.com/api/v1/channels/annoying',
+  'https://kick.com/api/v1/channels/nesszv',
+  'https://kick.com/api/v1/channels/adinross',
+  'https://kick.com/api/v1/channels/garydavid',
+]
