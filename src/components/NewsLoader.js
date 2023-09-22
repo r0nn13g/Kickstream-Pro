94e295d375234bd7c47202fa6f5412322fbfb2d6
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-22 01:22:28 -0400

    fixed youtube thumbnails, updated featured channel on news section

--- src/components/NewsLoader.js
+++ src/components/NewsLoader.js
@@ -6,7 +6,7 @@ import '../styles/video-styles.css';
 const NewsLoader = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-      axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&q=chadwinsmith&type=video&key=${process.env.REACT_APP_API_KEY}`)
+      axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&q=potatojet&type=video&key=${process.env.REACT_APP_API_KEY}`)
       .then((response) => {
                 setData(response.data.items);
             })
@@ -18,14 +18,14 @@ const NewsLoader = () => {
     const youtubeDataArr = data.map((video) => (
         <div key={video.id.videoId} className="youtube-thumbnail-wrapper">
             <Link className='youtube-thumbnail-container' to={`/videos/${video.id.videoId}`}>
-                <img id="youtube-thumbnails" src={video.snippet.thumbnails.high.url} alt="youtube thumbnail" />
+                <img id="youtube-thumbnails" src={video.snippet.thumbnails.medium.url} alt="youtube thumbnail" />
                 <h4 id="youtube-thumbnail-title">{video.snippet.title}</h4>
             </Link>
         </div>
     ));
   return(
     <div>
-         <h6 style={{color: "var(--green-elements)", backgroundColor: "rgb(40, 40, 40)", fontWeight: "900", textAlign: "center", margin: "0px"}}>News provided by @chadwinsmith</h6>
+         <h6 style={{color: "var(--green-elements)", backgroundColor: "rgb(40, 40, 40)", fontWeight: "900", textAlign: "center", margin: "0px"}}>News provided by @PotatoJet</h6>
             <div className="videos">
                 {youtubeDataArr}
             </div>
