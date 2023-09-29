14eb3e3b1aff47c2ac02d6b3035cd0fbfc85df70
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-28 23:20:31 -0400

    removed react Link form home page animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import '../styles/homepage-styles.css';
 import Footer from "../components/Footer";
 import kicksterLogo from "../assets/LSS.gif";
@@ -7,10 +6,8 @@ import kicksterLogo from "../assets/LSS.gif";
 const Home = () => {  
   return(
     <div className="'homepage-container'">
-      <div className="home-text" style={{marginTop: "180px"}}>
-          <Link to={'/live'}>
+      <div className="home-text" style={{marginTop: "180px"}}>   
             <img id="home-animation" src={kicksterLogo} alt="Live-streaming-solutions"/>
-          </Link>
           <div></div>
         </div>
           <Footer/>
