47abeacd396079614c1e0e7e4c434f36f19e6257
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-28 23:09:55 -0400

    initialized password protected page component

---
+++ src/components/PasswordProtected.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PasswordProtected = () => {
+  return (
+    <div className="password-protected-page-wrapper">
+        something
+    </div>
+)
+}
+
+export default PasswordProtected;
