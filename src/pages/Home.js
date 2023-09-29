bfe4b01d992e2a3f41f6b4f017a43d83758e5eaf
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-09-28 23:22:00 -0400

    imported PasswordProtected component on home page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../styles/homepage-styles.css';
 import Footer from "../components/Footer";
 import kicksterLogo from "../assets/LSS.gif";
+import PasswordProtected from "../components/PasswordProtected";
 
 const Home = () => {  
   return(
@@ -9,6 +10,7 @@ const Home = () => {
       <div className="home-text" style={{marginTop: "180px"}}>   
             <img id="home-animation" src={kicksterLogo} alt="Live-streaming-solutions"/>
           <div></div>
+          <PasswordProtected />
         </div>
           <Footer/>
     </div>
