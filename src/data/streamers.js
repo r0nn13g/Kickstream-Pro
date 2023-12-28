b079dddee417b28d28ba95a1495c9ae79e269957
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-12-28 15:57:25 -0500

    updated streamers list

--- src/data/streamers.js
+++ src/data/streamers.js
@@ -40,7 +40,7 @@ export let streamers = [
     'https://kick.com/api/v1/channels/sam',
     'https://kick.com/api/v1/channels/sampanday',
     'https://kick.com/api/v1/channels/shotime',
-    // 'https://kick.com/api/v1/channels/sjc_official',
+    'https://kick.com/api/v1/channels/sjc_official',
     'https://kick.com/api/v1/channels/slightlyhomeless',
     'https://kick.com/api/v1/channels/slykane',
     'https://kick.com/api/v1/channels/suspendas',
