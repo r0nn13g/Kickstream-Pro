fe21b6cac5c0433e6887a5b02ac6d7a1507139da
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2024-01-06 00:11:41 -0500

    updated streamers list

--- src/data/Streamers.js
+++ src/data/Streamers.js
@@ -10,6 +10,7 @@ export let streamers = [
     'https://kick.com/api/v1/channels/burgerplanet',
     'https://kick.com/api/v1/channels/brucedropemoff',
     'https://kick.com/api/v1/channels/carldo',
+    'https://kick.com/api/v1/channels/captaingee',
     'https://kick.com/api/v1/channels/crazytawn',
     'https://kick.com/api/v1/channels/deepak',
     'https://kick.com/api/v1/channels/destiny',
