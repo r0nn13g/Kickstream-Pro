ef34182b5d33fa06be148fec82a6e982c0381e32
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2024-01-12 22:40:32 -0500

    updated streamers list

--- src/data/Streamers.js
+++ src/data/Streamers.js
@@ -53,5 +53,5 @@ export let streamers = [
     'https://kick.com/api/v1/channels/totalirl',
     'https://kick.com/api/v1/channels/turanspidey',
     'https://kick.com/api/v1/channels/trainwreckstv',
-    // 'https://kick.com/api/v1/channels/xenathewitch',
+    'https://kick.com/api/v1/channels/xenathewitch',
 ];
