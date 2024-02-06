560a5ab7a0ebdc67e69fb7617409d00bfa9d6073
R <ronniequest6@gmail.com>
2024-02-06 14:12:52 -0800

    updated year on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return(
     <footer className="footer">
       <p id='footer-time' >Arrival: {now.getHours()}:{now.getUTCMinutes()}:{now.getUTCSeconds()}</p >
-      <p id="footer-text">2023 Kickstream Pro. ALL RIGHTS RESERVED</p>
+      <p id="footer-text">2024 Kickstream Pro. ALL RIGHTS RESERVED</p>
     </footer>
   )
 }
