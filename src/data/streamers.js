0046daca9c7479474710be2c807199936d6a1691
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2025-02-27 15:25:32 -0500

    updated streamers

--- src/data/streamers.js
+++ src/data/streamers.js
@@ -2,6 +2,7 @@ export let channels = [
     'https://kick.com/api/v1/channels/abz',
     'https://kick.com/api/v1/channels/ac7ionman',
     'https://kick.com/api/v1/channels/adinross',
+    'https://kick.com/api/v1/channels/adrianahlee',
     'https://kick.com/api/v1/channels/alexis',
     'https://kick.com/api/v1/channels/andy',
     'https://kick.com/api/v1/channels/asianandy',
